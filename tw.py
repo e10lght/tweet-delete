cd3e6600521f404b507d86e818da33735f0548e8
sw-itok <itok@solutionware.jp>
2023-06-01 01:13:53 +0900

    コメントアウト外し

--- tw.py
+++ tw.py
@@ -38,6 +38,7 @@ try:
 
     time.sleep(3)
 
+    # 返信タブに移動
     replay = driver.find_elements(By.CSS_SELECTOR, '.css-1dbjc4n.r-16y2uox.r-6b64d0.r-cpa5s6')
     if len(replay) > 1:  
         fourth_element = replay[1] 
@@ -45,21 +46,22 @@ try:
     else:
         print("Element not found or less than two elements")
 
-    # for i in range(500):
-    #     print(str(i)+"start")
-    #     time.sleep(1)
-    #     elements = driver.find_elements(By.CSS_SELECTOR, '.css-18t94o4.css-1dbjc4n.r-1777fci.r-bt1l66.r-1ny4l3l.r-bztko3.r-lrvibr')
-    #     if len(elements) > 0:
-    #         elements[0].click()
-    #         time.sleep(1)
-    #         delete = driver.find_elements(By.CSS_SELECTOR, '.css-1dbjc4n.r-1loqt21.r-18u37iz.r-1ny4l3l.r-ymttw5.r-1f1sjgu.r-o7ynqc.r-6416eg.r-13qz1uu')
-    #         delete[0].click()
-    #         time.sleep(1)
-    #         submit = driver.find_elements(By.CSS_SELECTOR, '.css-901oao.r-1awozwy.r-jwli3a.r-6koalj.r-18u37iz.r-16y2uox.r-1tl8opc.r-a023e6.r-b88u0q.r-1777fci.r-rjixqe.r-bcqeeo.r-q4m81j.r-qvutc0')
-    #         submit[0].click()
-    #         time.sleep(1)
-    #     else:
-    #         print("Element not found")
+    # 削除実行（リツイートは不可）
+    for i in range(500):
+        print(str(i)+"start")
+        time.sleep(1)
+        elements = driver.find_elements(By.CSS_SELECTOR, '.css-18t94o4.css-1dbjc4n.r-1777fci.r-bt1l66.r-1ny4l3l.r-bztko3.r-lrvibr')
+        if len(elements) > 0:
+            elements[0].click()
+            time.sleep(1)
+            delete = driver.find_elements(By.CSS_SELECTOR, '.css-1dbjc4n.r-1loqt21.r-18u37iz.r-1ny4l3l.r-ymttw5.r-1f1sjgu.r-o7ynqc.r-6416eg.r-13qz1uu')
+            delete[0].click()
+            time.sleep(1)
+            submit = driver.find_elements(By.CSS_SELECTOR, '.css-901oao.r-1awozwy.r-jwli3a.r-6koalj.r-18u37iz.r-16y2uox.r-1tl8opc.r-a023e6.r-b88u0q.r-1777fci.r-rjixqe.r-bcqeeo.r-q4m81j.r-qvutc0')
+            submit[0].click()
+            time.sleep(1)
+        else:
+            print("Element not found")
 
     input("Press any key to close the browser...")
 
